643ceacf8334f1df4c775962c9f0ce291af7b2bf
Wijna <15200058@bsi.ac.id>
2022-03-22 15:29:19 +0700

    init

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller {
+    public function index(){
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Wijna Adhiguna Wijaya Putra
+            Saya tinggal di daerah Rawa Sari
+            Hobi saya adalah bermain videogames";
+    }
+}
