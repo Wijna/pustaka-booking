b05374e0d11d115cacb4b09d71162f37d4e355bd
Wijna <15200058@bsi.ac.id>
2022-04-05 16:58:28 +0700

    init

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan : Wijna</li>
+		<li>Nama Belakang : Putra</li>
+	</ul>
+	<br>
+	<h4>Alamat</h4>
+	<ul type="none">
+		<li> Jalan Daksinapat Timur C No.12 Rawamangun </li>
+	</ul>
+
+	<h4>Tempat Lahir</h4>
+	<ul type="none">
+		<li>Jakarta</li>
+	</ul>
+
+	<h4>Hobi</h4>
+	<ul type="square">
+		<li>Bermain Videogames</li>
+	</ul>
+</section>
